6112d33775f4dfd4289babd47e25747f4032fcbc
Jeff Mitchell <sentinel1909@protonmail.com>
2022-10-16 09:51:13 -0700

    Tweak to card field names

--- src/lib/domain/card.rs
+++ src/lib/domain/card.rs
@@ -8,7 +8,7 @@ pub struct Card {
     pub id: u32,
     pub title: &'static str,
     pub date: &'static str,
-    pub content: &'static str,
+    pub excerpt: &'static str,
 }
 
 impl Card {
@@ -18,37 +18,37 @@ impl Card {
                 id: 20220405,
                 title: "Rust: The Journey Begins",
                 date: "2022-04-05",
-                content: "Since the end of 2020, I've been on a mission to re-learn to code...",
+                excerpt: "Since the end of 2020, I've been on a mission to re-learn to code...",
             },
             Card {
                 id: 20221010,
                 title: "Second Post",
                 date: "2022-10-10",
-                content: "Getting up and running with Rocket is easy...",
+                excerpt: "Getting up and running with Rocket is easy...",
             },
             Card {
                 id: 20221011,
                 title: "Third Post",
                 date: "2022-10-11",
-                content: "OMG it took me 2 hours to render these cards...",
+                excerpt: "OMG it took me 2 hours to render these cards...",
             },
             Card {
                 id: 20221012,
                 title: "Fourth Post",
                 date: "2022-10-12",
-                content: "Trying to get markdown rendered to a posts page...",
+                excerpt: "Trying to get markdown rendered to a posts page...",
             },
             Card {
                 id: 20221013,
                 title: "Fifth Post",
                 date: "2022-10-13",
-                content: "Just filling in space...",
+                excerpt: "Just filling in space...",
             },
             Card {
                 id: 20221014,
                 title: "Sixth Post",
                 date: "2022-10-14",
-                content: "And now the grid is complete...",
+                excerpt: "And now the grid is complete...",
             },
         ];
         cards
